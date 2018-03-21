d405e50e5780b45c5a5dc045daf868c058f70be7
Martyn Dewar <martyn+github@dewarfamily.org.uk>
2018-03-21 11:03:59 +0000

    bugfix

--- british.php
+++ british.php
@@ -3,6 +3,7 @@ $servername = "mysql-server-1";
 $username = "cmh1";
 $password = "abccmh1354";
 $DB_Name = "cmh1";
+$cat= $_GET['cat'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $DB_Name);
@@ -12,14 +13,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT recipe_name, recipe_image FROM Recipe WHERE food_category_id = 'british'";
+$sql = "SELECT recipe_name, recipe_image FROM Recipe WHERE food_category_id = '$cat'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       echo '
         <div align="center" class ="center">
-            <a href="'. $row["recipe_name"]. '"><img src="'. $row["recipe_image"]. '" alt="'. $row["recipe_name"]. '"  width="220" height="170"></img>
+            <a href="rdis.php?rid='. $row["recipe_id"]. '"><img src="'. $row["recipe_image"]. '" alt="'. $row["recipe_name"]. '"  width="220" height="170"></img>
             <p> '. $row["recipe_name"]. ' </p></a>
         </div>
       ';

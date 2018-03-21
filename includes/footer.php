f2408e40b36d17d4ec8644c8deb5871969959c50
Martyn Dewar <martyn+github@dewarfamily.org.uk>
2018-03-21 07:54:42 +0000

    bug

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
-Copyright © 2018 Delectamenti. Delectamenti is not responsible for any allergic reactions caused by the consumption of our recipes. <a href="https://www.fda.gov/Food/ResourcesForYou/Consumers/ucm079311.htm"> Please check this FDA document on food allergies if unsure. </a>
+Copyright © 2018 Delectamenti. We are responsible for any allergic reactions caused by the consumption of our recipes. <a href="https://www.fda.gov/Food/ResourcesForYou/Consumers/ucm079311.htm"> Please check this FDA document on food allergies if unsure. </a>
 Recipes for this website have been used under the non-commercial research and private study exception to the Copyright. All recipes originally sourced from BBC Food. <?php
-echo "Today is " . date("l, d MM y");
-echo "Created date is " . date("Y-m-d h:i:sa", $d);
+echo "Today is " . date("l, d M y");
 ?>

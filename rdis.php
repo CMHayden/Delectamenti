c80f978f4bb175d14cd7e83ac026026f70f753ab
Martyn Dewar <martyn+github@dewarfamily.org.uk>
2018-03-21 04:03:55 +0000

    proto

--- rdis.php
+++ rdis.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <meta charset="UTF-8"></meta>
-    <meta name="description" content="A website gathering recipes and helping to teach people how to cook."></meta>
-    <meta name="keywords" content="Delectamenti. Food. Eat. Cook. Recipe."></meta> <!-- used by search engines -->
-    <meta name="author" content="Callum M Hayden & Martyn Dewar"></meta>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> <!-- used for RWD -->
+    <meta name="description" content="A website gathering recipes and helping to teach people how to cook.">
+    <meta name="keywords" content="Delectamenti. Food. Eat. Cook. Recipe."> <!-- used by search engines -->
+    <meta name="author" content="Callum M Hayden & Martyn Dewar">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- used for RWD -->
     <link rel="stylesheet" media="screen and (min-width: 550px)" href="style.css"></link>
     <link rel="stylesheet" media="screen and (max-width: 550px)" href="smallstyle.css"></link>
 		<link rel="stylesheet" href="temp.css">
@@ -22,16 +22,17 @@
         window.print();
     }
     </script>
-	<!--shares -->
-	<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5ab1d5f2b33883001365505d&product=inline-share-buttons"></script>
-	<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+	<!--start social media and sharing code -->
+		<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5ab1d5f2b33883001365505d&product=inline-share-buttons"></script>
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12';
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+	<!-- end social media and sharing code -->
 </head>
 <body>
 <div class = "wrapper" id="wrapper">
@@ -46,56 +47,59 @@
       <input type="text" name="search" placeholder="Search.."></input>
     </form>
   </div>
-  <div class = "logohead" id="logohead">
-    <a href="index.html"><h1>Delectamenti</h1></a>
+  <div class = "logo" id="logo">
+    <a href="index.php"><h1>Delectamenti</h1></a>
   </div>
   <div class = "nav" id="myNav">
-    <a                  href="index.html">    Home    </a>
-    <a></a>
-    <a                  href="about.html">    About   </a>
-    <a></a>
-    <a                  href="contact.html">  Contact </a>
-    <a></a>
-    <a                  href="login.html">    Login   </a>
+    <nav>
+		<a                  href="index.php">    Home    </a>
+		<a></a>
+		<a                  href="about.html">    About   </a>
+		<a></a>
+		<a                  href="contact.html">  Contact </a>
+		<a></a>
+		<a                  href="login.html">    Login   </a>
+	  </nav>
   </div>
     <div class = "recipe">
-      <p>Vegetable pad Thai</p><br/>
-      <img src="Image/vegetablepadthai.png" alt="Vegetable pad Thai" height="155px" width="220px" padding="15px" align = "left"></img>
-      Ingredients: <br></br>
-      200g (7oz) flat rice noodles<br />
-      4 tbsp tamarind sauce<br />
-      2 tbsp fish sauce<br />
-      1 tbsp brown sugar<br />
-      1/2-1 tsp tsp chilli flakes<br />
-      2 tbsp vegetable oil<br />
-      2 garlic cloves, finely chopped<br />
-      2 large eggs, lightly beaten<br />
-      1 x 320g pack vegetable and beansprout stir fry<br />
-      50g (2oz) roasted peanuts, roughly chopped<br />
-      1 lime, quartered<br />
-      <br />
-      Directions:
-      <ol>
-        <li>Soak the noodles in freshly boiled water for 12 minutes. Strain and set aside. While the
-          noodles are soaking, mix the tamarind, fish sauce, sugar and chilli flakes together.</li>
-        <li>Heat the vegetable oil in a large frying pan and add the garlic. Cook briefly, watching it
-          doesn’t burn, then add the tamarind and fish sauce mixture. Let this bubble for a minute,
-          then transfer to a bowl.</li>
-        <li>In the same pan, on medium-high heat, add the beaten eggs. Let them set for a minute,
-          without stirring. Once set, break vigorously with a wooden spoon.</li>
-        <li>Add the stir fry vegetables and cook for 2 minutes, stirring from time to time. Then
-          add the noodles, pour over the sauce, stir well and cook for another minute or two. Serve
-          sprinkled with chopped peanuts and a squeeze of lime.</li>
-      </ol>
+	<section aria-label="recipe" id="recipe">
+		  <p>Vegetable pad Thai</p><br/>
+		  <img src="Image/vegetablepadthai.png" alt="Vegetable pad Thai" height="155px" width="220px" padding="15px" align = "left"></img>
+		  Ingredients: <br></br>
+		  200g (7oz) flat rice noodles<br />
+		  4 tbsp tamarind sauce<br />
+		  2 tbsp fish sauce<br />
+		  1 tbsp brown sugar<br />
+		  1/2-1 tsp tsp chilli flakes<br />
+		  2 tbsp vegetable oil<br />
+		  2 garlic cloves, finely chopped<br />
+		  2 large eggs, lightly beaten<br />
+		  1 x 320g pack vegetable and beansprout stir fry<br />
+		  50g (2oz) roasted peanuts, roughly chopped<br />
+		  1 lime, quartered<br />
+		  <br />
+		  Directions:
+		  <ol>
+			<li>Soak the noodles in freshly boiled water for 12 minutes. Strain and set aside. While the
+			  noodles are soaking, mix the tamarind, fish sauce, sugar and chilli flakes together.</li>
+			<li>Heat the vegetable oil in a large frying pan and add the garlic. Cook briefly, watching it
+			  doesn’t burn, then add the tamarind and fish sauce mixture. Let this bubble for a minute,
+			  then transfer to a bowl.</li>
+			<li>In the same pan, on medium-high heat, add the beaten eggs. Let them set for a minute,
+			  without stirring. Once set, break vigorously with a wooden spoon.</li>
+			<li>Add the stir fry vegetables and cook for 2 minutes, stirring from time to time. Then
+			  add the noodles, pour over the sauce, stir well and cook for another minute or two. Serve
+			  sprinkled with chopped peanuts and a squeeze of lime.</li>
+		  </ol>
+	</section>
       <!--<button onclick="myFunction()">Print this recipe</button>-->
 		<div class="sharethis-inline-share-buttons"></div>
       <br />
       <br />
     </div>
 	<section>
-		<div class="fb-comments" data-href="http://www2.macs.hw.ac.uk/~md46/F28CD/cw/Delectamenti/rdis.php" data-numposts="5"></div>	
+		<div class="fb-comments" data-href="http://www2.macs.hw.ac.uk/~md46/F28CD/cw/Delectamenti/rdis.php?rid=" data-numposts="5"></div>	
 	</section>
-  </body>
     <footer>
       <div class ="footer" id="footer">
         Copyright © 2018 Delectamenti. Delectamenti is not responsible for any allergic reactions caused by the consumption of our recipes. <a href="https://www.fda.gov/Food/ResourcesForYou/Consumers/ucm079311.htm"> Please check this FDA document on food allergies if unsure. </a>
@@ -103,4 +107,5 @@
         Any enquiries please use the contact form or email us at <a href="mailto:delectamentiuk@gmail.com?Subject=Enquiry" target="_top">DelectamentiUK@gmail.com</a> .
       </div>
     </footer>
+  </body>
 </html>
